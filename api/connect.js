9190dbc880ca9408c840cc1456953e230834376b
Zainab Jafari <zainabjafari998@gmail.com>
2024-09-27 12:54:59 +0200

    vercel

--- api/connect.js
+++ api/connect.js
@@ -1,16 +1,24 @@
 import mysql from 'mysql';
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Jaza1998?',
-    database: 'rentcarapi'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USERNAME);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_DBNAME);
+
+export const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
 });
-// Anslut till MySQL-databasen
-db.connect((err) => {
+
+db.connect(err => {
     if (err) {
-        console.error('Kunde inte ansluta till databasen:', err);
+        console.error('Database connection failed: ' + err.stack);
         return;
     }
-    console.log('Ansluten till MySQL-databasen');
+    console.log('Connected to database.');
 });
-export { db };
